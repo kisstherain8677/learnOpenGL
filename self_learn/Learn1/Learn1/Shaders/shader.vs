bc3dadb4a024e76a40ef2379089e5686a928c985
hao <842658636@qq.com>
2020-06-06 21:18:31 +0800

    加入两个纹理，用一个uniform变量实现纹理交替混合动画

--- self_learn/Learn1/Learn1/Shaders/shader.vs
+++ self_learn/Learn1/Learn1/Shaders/shader.vs
@@ -1,14 +1,14 @@
-
 #version 330 core
-layout (location=0) in vec3 aPos;//位置变量的属性位置值为0
-layout (location=1) in vec3 aColor;//颜色变量的属性位置值为1 
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aTexCoord;
+
 out vec3 ourColor;
-uniform float xOffset;
-void main(){
-//gl_Position=vec4(aPos,1.0);
-//上下颠倒
-gl_Position=vec4(aPos.x+xOffset,-aPos.y,aPos.z,1.0);
-//ourColor=aColor;颜色根据输入
-//颜色与顶点相等
-ourColor=aPos;
+out vec2 TexCoord;
+
+void main()
+{
+    gl_Position = vec4(aPos, 1.0);
+    ourColor = aColor;
+    TexCoord = aTexCoord;
 }
--- self_learn/Learn1/Learn1/main.cpp
+++ self_learn/Learn1/Learn1/main.cpp
@@ -1,16 +1,10 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include"Shader.h"
+#include"stb_image.h"
 #include<iostream>
 
 
-//顶点属性
-float vertices[] = {
-	//位置            //颜色
-	-0.5f,-0.5f,0.0f,1.0f,0.0f,0.0f,//右下
-	 0.5f,-0.5f,0.0f,0.0f,1.0f,0.0f,//左下
-	 0.0f,0.5f,0.0f,0.0f,0.0f,1.0f//顶点
-};
 
 
 
@@ -35,6 +29,8 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);//opengl的次版本号
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);//使用opengl的核心模式
 
+
+
 	//创建窗口对象
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 	if (window == NULL) {
@@ -52,31 +48,99 @@ int main() {
 
 	//设置viewport
 	//viewport是告诉opengl的，最后根据这个进行2维映射处理
-	glViewport(0, 0, 800, 600);
+	//glViewport(0, 0, 800, 600);
 	//viewport随着窗口改变的回调函数
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 	//创建着色器对象
 	Shader ourShader("Shaders/shader.vs", "Shaders/shader.fs");
 
-	unsigned int VBO;
-	//生成缓冲对象
-	glGenBuffers(1, &VBO);
-	
-	unsigned int VAO;//VBO数据和顶点属性配置
+	//顶点属性
+	float vertices[] = {
+		//     ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
+			 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // 右上
+			 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 右下
+			-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // 左下
+			-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // 左上
+	};
+
+	//索引列表
+	unsigned int indices[] = {
+		0,1,3,//第一个三角形
+		1,2,3//第二个三角形
+	};
+
+
+	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
 	//绑定VAO
 	glBindVertexArray(VAO);
 	//把顶点数组复制到缓冲中供opengl使用
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	//复制索引数组到一个索引缓冲中
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	//解释顶点数据以应用到顶点着色器的逐个顶点属性（设置顶点属性指针）
 	//位置属性
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 	//颜色属性
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
+	//纹理坐标属性
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	//生成纹理
+	unsigned int texture1, texture2;
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+	//为当前绑定的纹理对象设置环绕、过滤方式
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	//加载并生成纹理
+	int width, height, nrChannels;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* data = stbi_load("pic/container.jpg", &width, &height, &nrChannels, 0);
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else {
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+	//第二张纹理
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+	//为当前绑定的纹理对象设置环绕、过滤方式
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	//加载并生成纹理
+	data = stbi_load("pic/awesomeface.png", &width, &height, &nrChannels, 0);
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else {
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+
+	ourShader.use();//set uniform之前要
+	glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+	glUniform1i(glGetUniformLocation(ourShader.ID, "texture2"), 1);
+	//ourShader.setInt("texture2", 1);
+	//渲染循环
 	while (!glfwWindowShouldClose(window)) {
 
 		//处理输入
@@ -87,17 +151,29 @@ int main() {
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);//设置清空缓冲的颜色（状态设置函数）
 		glClear(GL_COLOR_BUFFER_BIT);//清空颜色缓冲（状态使用函数。OpenGL是一个大的状态机）
 
+		//绑定纹理单元
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);//会绑定到激活的纹理单元
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
+
+
 		ourShader.use();
-		ourShader.setFloat("xOffset", 0.5f);
-		
-		//绘制三角形
+		float timeValue = glfwGetTime();
+		float mixa = (sin(timeValue*2.0*3.14159)+1)*0.5f;
+		ourShader.setFloat("mixa", mixa);
+		//绘制
 		glBindVertexArray(VAO);//使用绑定的VAO（里面有顶点属性信息）
-		glDrawArrays(GL_TRIANGLES, 0, 3);//绘制三角形命令
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);//是从索引缓冲中渲染 不同于glDrawArray
 
 		//检查并调用事件，交换缓冲。
 		glfwSwapBuffers(window);//交换颜色缓冲。是一个储存每个像素颜色值的大缓冲
 		glfwPollEvents();//检查有无触发事件
 	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 
 	//渲染结束后释放所有的glfw资源
 	glfwTerminate();
