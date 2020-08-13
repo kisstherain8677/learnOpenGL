c986d27dc6e67d2883bdee119b3e3415cce0c89f
hao <842658636@qq.com>
2020-08-13 23:42:00 +0800

    平行光和点光

--- self_learn/Learn1/Learn1/Shaders/1.colors.fs
+++ self_learn/Learn1/Learn1/Shaders/1.colors.fs
@@ -8,12 +8,19 @@ struct Material{//各个反射方式的反射颜色，和材料相关
 
 uniform Material material;
 
-struct Light{//各个反射方式的强度，和光源相关。三个值相同，因为只是表示强度
+struct Light{
 	vec3 position;
+	vec3 direction;
 
 	vec3 ambient;
 	vec3 diffuse;
 	vec3 specular;
+
+	//衰减三系数
+	float constant;//常数
+	float linear;
+	float quadratic;
+
 };
 uniform Light light;
 
@@ -23,21 +30,23 @@ in vec2 TexCoords;
 in vec3 Normal;  
 in vec3 FragPos;  
 
-  
-uniform vec3 lightPos; 
-uniform vec3 lightColor;
-uniform vec3 objectColor;
+
 uniform vec3 viewPos;
 
 void main()
 {
+
+    //attenuation
+	float distance=length(light.position-FragPos);
+	float attenuation=1.0/(light.constant+light.linear*distance+light.quadratic*(distance*distance));
+
     // ambient
     
     vec3 ambient =light.ambient * vec3(texture(material.diffuse,TexCoords));
   	
     // diffuse 
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
+    vec3 lightDir = normalize(light.position-FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = light.diffuse*diff*vec3(texture(material.diffuse,TexCoords));
 
@@ -49,6 +58,6 @@ void main()
 	float spec=pow(max(dot(viewDir,reflectDir),0.0),shineness);
 	vec3 specular=light.specular*spec*vec3(texture(material.specular,TexCoords));
             
-    vec3 result = ambient + diffuse+specular;
+    vec3 result = ambient*attenuation + diffuse*attenuation+specular*attenuation;
     FragColor = vec4(result, 1.0);
 } 
